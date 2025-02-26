1ab6256267a1de1fb068b66733eefc09f134350b
JosDev25 <joseph.rodriguez@valere.io>
2025-02-26 13:00:23 -0500

    #3 feat(model): Added new classes to handle future implementations

--- model.py
+++ model.py
@@ -1,4 +1,14 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field
 
 class Model(BaseModel):
-    text: str=Field(min_length=10,max_length=400)
+    text: str=Field(min_length=10,max_length=1000)
+
+class User(BaseModel):
+    name: str=Field(min_length=5, max_length=100)
+    email: EmailStr 
+    api_calls: int=0
+    chats: list[str]=[]
+    
+class AnonymousSession(BaseModel):
+    session_id: str
+    api_calls: int = 0  
