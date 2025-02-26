99daabdf5c0c7a8f33c0e9e31a60ad7e86b95561
JosDev25 <joseph.rodriguez@valere.io>
2025-02-26 15:29:48 -0500

    #4 feat(db_manager): new added function and get user to retrieve data from the DB(Already connected)

--- db_manager.py
+++ db_manager.py
@@ -17,3 +17,34 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+    
+db=client["chatbot_db"]
+users_collection = db["users"]
+anonymous_sessions_collection = db["anonymous_sessions"]
+
+def add_user(name, email):
+    existing_user=users_collection.find_one({"email": email})
+    
+    if existing_user:
+        return {"message": "User already exist"}
+    new_user={
+        "name": name,
+        "email": email,
+        "api_calls": 0,
+        "chats": []
+    }
+    
+    users_collection.insert_one(new_user)
+    
+    return{"message": "User created succesfully"}
+
+def get_user(email):
+    user = users_collection.find_one({"email": email}, {"_id": 0})  
+    
+    if user:
+        return user
+    else:
+        return{"message": "User not found"}
+    
+    
+
