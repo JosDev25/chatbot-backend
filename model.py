c7edbaee98c41efdcf13c9d286c069f6af62ece2
JosDev25 <joseph.rodriguez@valere.io>
2025-02-27 10:02:25 -0500

    #6 fix(model): updated to dictionary instead of list, to handle user and bot difference as key value

--- model.py
+++ model.py
@@ -4,11 +4,12 @@ class Model(BaseModel):
     text: str=Field(min_length=10,max_length=1000)
 
 class User(BaseModel):
-    name: str=Field(min_length=5, max_length=100)
+    name: str = Field(min_length=5, max_length=100)
     email: EmailStr 
-    api_calls: int=0
-    chats: list[str]=[]
+    api_calls: int = 0
+    chats: list[dict] = []
     
 class AnonymousSession(BaseModel):
     session_id: str
-    api_calls: int = 0  
+    api_calls: int = 0
+    chats: list[dict] = []
