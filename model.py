faee88584e1e949e4b743b90d668e9c0f374df9f
JosDev25 <joseph.rodriguez@valere.io>
2025-02-28 14:50:07 -0500

    #17 feat(model): Added Login class and password attributes for the User

--- model.py
+++ model.py
@@ -6,8 +6,13 @@ class ChatRequest(BaseModel):
 class User(BaseModel):
     name: str = Field(min_length=5, max_length=100)
     email: EmailStr 
+    password: str = Field(min_length=8) 
     api_calls: int = 0
     chats: list[dict] = []
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str 
     
 class AnonymousSession(BaseModel):
     session_id: str
