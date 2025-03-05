4a78b66877e3b6221514965c601cb8c6c8e531cb
JosDev25 <joseph.rodriguez@valere.io>
2025-03-05 09:34:37 -0500

    #19 feat(db_manager): Added hash function with hashlib to secure password storage

--- db_manager.py
+++ db_manager.py
@@ -1,8 +1,10 @@
+
+import hashlib
 import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-
+from passlib.context import CryptContext
 
 load_dotenv()
 
@@ -22,27 +24,38 @@ db=client["chatbot_db"]
 users_collection = db["users"]
 anonymous_sessions_collection = db["anonymous_sessions"]
 
-def add_user(name, email, session_id=None):
-    existing_user=users_collection.find_one({"email": email})
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def add_user(name, email, password, session_id=None):
+    existing_user = users_collection.find_one({"email": email})
     
     if existing_user:
         return {"message": "User already exist"}
-    new_user={
+    
+    hashed_password = pwd_context.hash(password)
+    
+    new_user = {
         "name": name,
         "email": email,
+        "password": hashed_password,  
         "api_calls": 0,
         "chats": []
     }
+    
     if session_id:
         session = anonymous_sessions_collection.find_one({"session_id": session_id})
         if session and "chats" in session:
             new_user["chats"] = session["chats"] 
             anonymous_sessions_collection.delete_one({"session_id": session_id})  
-
             
     users_collection.insert_one(new_user)
-    
-    return{"message": "User created succesfully and chat history transferred" if session_id else "User created"}
+     
+    return {"message": "User created succesfully and chat history transferred" if session_id else "User created"}
 
 def get_user(email):
     user = users_collection.find_one({"email": email}, {"_id": 0})  
